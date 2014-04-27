b63a5b21a01318e90ba919fef3bf04768980fdea
ash7779 <ash7779@gmail.com>
2014-04-27 01:37:59 -0400

    Test commit from mac

--- RealTimeDomain/src/org/dadala/rtdomain/GitTest.java
+++ RealTimeDomain/src/org/dadala/rtdomain/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Git is funny...");
+		System.out.println("Git is funniest...");
 	}
 
 }
